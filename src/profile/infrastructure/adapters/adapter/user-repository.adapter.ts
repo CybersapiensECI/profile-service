211cca4e26aade40a67cdb60295173ea0ffa4680
Andrew7480 <andrewc7480@gmail.com>
2026-06-18 18:09:22 -0500

    fixing workflows

--- src/profile/infrastructure/adapters/adapter/user-repository.adapter.ts
+++ src/profile/infrastructure/adapters/adapter/user-repository.adapter.ts
@@ -8,7 +8,6 @@ import { UserNotFoundException } from '../../../domain/exceptions/user-not-found
 import { UserRepositoryPort } from '../../../domain/ports/out/user-repository.port';
 import { OrganizerDocument } from '../persistence/entity/organizer.document';
 import { StudentDocument } from '../persistence/entity/student.document';
-import type { UserDocument } from '../persistence/entity/user.document';
 import { UserType } from '../persistence/entity/user-type.enum';
 import { UserPersistenceMapper } from '../persistence/mapper/user-persistence.mapper';
 import { UserMongoRepository } from '../persistence/repository/user-mongo.repository';
@@ -33,7 +32,7 @@ export class UserRepositoryAdapter implements UserRepositoryPort {
       const saved = await this.userMongoRepository.save(
         this.userMapper.adminToDocument(user),
       );
-      return this.userMapper.adminToDomain(saved as unknown as UserDocument);
+      return this.userMapper.adminToDomain(saved);
     }
     if (user instanceof Organizer) {
       const saved = await this.userMongoRepository.save(
@@ -72,7 +71,7 @@ export class UserRepositoryAdapter implements UserRepositoryPort {
         this.userMapper.adminToDocument(user),
       );
       if (!saved) throw new UserNotFoundException(`User ${id} not found`);
-      return this.userMapper.adminToDomain(saved as unknown as UserDocument);
+      return this.userMapper.adminToDomain(saved);
     }
     if (user instanceof Organizer) {
       const saved = await this.userMongoRepository.update(
